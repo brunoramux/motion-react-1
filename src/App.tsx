a78f0b184ec3b5078bde97af1af03b118bade6ae
brunoramux <bruno.lemos@live.com>
2024-04-13 20:50:31 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './styles/global'
+import { Main, Section, Sticky } from './styles';
+import FirstAndSecond from './components/FirstAndSecond';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+
+      <Main style={{ height: '1610vh '}}>
+        <Section style={{height: '72.7%'}}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7% '}}>
+          <Sticky className='third'/>
+        </Section>
+
+        <Section style={{ height: '10.1% '}}>
+          <Sticky className='fourth'/>
+        </Section>
+      </Main>
+    </>
   );
 }
 
